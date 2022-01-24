5332c618de7fa8b1137c5f6890268e263c26092d
githubbestpeers <poingle@bestpeers.com>
2022-01-24 19:35:45 +0530

    performing operation on images

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_093139) do
+ActiveRecord::Schema.define(version: 2022_01_20_115615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.bigint "price"
+    t.string "author"
+    t.string "book_no"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
